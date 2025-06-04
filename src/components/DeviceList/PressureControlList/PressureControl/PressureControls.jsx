795e1dd6047e0434e9d3b3b31776c979153d5d56
duckies <voducminh39@gmail.com>
2025-06-04 15:32:51 +0700

    update

--- src/components/DeviceList/PressureControlList/PressureControl/PressureControls.jsx
+++ src/components/DeviceList/PressureControlList/PressureControl/PressureControls.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import val_on from "/svgs/val_on.svg";
 import val_off from "/svgs/val_off.svg";
 import Spinner from "../../../../commons/Spinner";
 import { Pencil, RefreshCcw } from "lucide-react";
 
 export default function PressureControl(props) {
-  // const { id, battery } = data;
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
 
@@ -16,6 +15,11 @@ export default function PressureControl(props) {
       state: props.data,
     });
   };
+
+  const location = useLocation();
+  const data = location.state || {};
+  console.log("id", data.id);
+
   const handleConnect = () => {
     setLoading(true);
     setTimeout(() => {
@@ -26,7 +30,6 @@ export default function PressureControl(props) {
   const handleEdit = () => {
     navigate(`/main/devices/pressureControl/1/edit`);
   };
-  console.log("data", props.data);
 
   const [valStatus, setValStatus] = useState({
     val_1: false,
@@ -41,35 +44,37 @@ export default function PressureControl(props) {
         {/* ============ */}
 
         <div>
-          <div className="flex justify-between items-center my-1 border rounded p-2">
-            <h1>1008</h1>
-            <button
-              onClick={handleClick}
-              type="button"
-              className="w-[100px] px-6 py-3.5 text-base font-medium text-white bg-green-600"
-            >
-              Enter
-            </button>
-          </div>
-
-          <div className="flex justify-between items-center ">
-            <h1>1008</h1>
-            <div
-              onClick={handleConnect}
-              className="border rounded ml-30 flex gap-2 items-center cursor-pointer hover:bg-gray-200 p-2 rounded-md"
-            >
-              {loading ? <Spinner /> : <RefreshCcw />}
-              <span>call</span>
-              <span className="">1:15</span>
+          {!data.id ? (
+            <div className="flex justify-between items-center my-1 border rounded p-2">
+              <h1>1008</h1>
+              <button
+                onClick={handleClick}
+                type="button"
+                className="w-[100px] px-6 py-3.5 text-base font-medium text-white bg-green-600"
+              >
+                Enter
+              </button>
             </div>
+          ) : (
+            <div className="flex justify-between items-center ">
+              <h1>1008</h1>
+              <div
+                onClick={handleConnect}
+                className="border rounded ml-30 flex gap-2 items-center cursor-pointer hover:bg-gray-200 p-2 rounded-md"
+              >
+                {loading ? <Spinner /> : <RefreshCcw />}
+                <span>call</span>
+                <span className="">1:15</span>
+              </div>
 
-            <div
-              onClick={handleEdit}
-              className="cursor-pointer active:scale-95 transition-all border border-gray-300 hover:border-blue-500 p-2 rounded"
-            >
-              <Pencil />
+              <div
+                onClick={handleEdit}
+                className="cursor-pointer active:scale-95 transition-all border border-gray-300 hover:border-blue-500 p-2 rounded"
+              >
+                <Pencil />
+              </div>
             </div>
-          </div>
+          )}
         </div>
 
         {/* =============== */}
